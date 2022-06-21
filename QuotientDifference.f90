f63e1f808bb5f7140f0898050d531435744e9948
Joaquin <joaquinmartinezfranco@gmail.com>
2022-06-21 17:09:06 -0300

    Add files via upload

--- QuotientDifference.f90
+++ QuotientDifference.f90
@@ -5,17 +5,41 @@ Program QuotientDifference
 !3- Comienza QD
     !i-Ejecuta primeras iteraciones
 
+IMPLICIT NONE
 
-Implicit none
-
-Integer:: n, i, iter, maxiter
-Real, allocatable:: a(0:), e(:), q(:)
-Real:: error, tol
+integer:: Grado, i ,Metodo
+real(8), allocatable, dimension(:) :: VecCoeficientes,VecRaices
+real(8) tol
 
 ! COMIENZA EL PROGRAMA
 
-CALL ingresa_coeficientes(i,n,a)
+CALL LeeCoeficientes(VecCoeficientes,Grado)
+!----------ERROR--------!
+write(*,*)"Ingrese la cota maxima de error" 
+read(*,*)tol
+!------Iteraciones-----!
+write(*,*)"Ingrese maxima cantidad de iteraciones" 
+read(*,*)maxiter
 
+
+do while (CoeficienteCero(VecCoeficientes,Grado) .AND. Metodo<4) 
+
+	Metodo=Metodo+1
+	CALL PreProcesamiento(VecCoeficientes,Grado,Metodo,Diferencia) !hay 3 metodos para corregir 0, haciendo diferentes reempazos s=x+d s+cx s=1/x
+  
+enddo
+
+if (Metodo>=4)
+
+  write(*,*)"Imposible obtener raices del polinomio : No fue posible modificarlo para que sus coeficientes fueran distintos de 0")
+  
+ else
+ 
+	VecRaices=0
+	call QuotientDifferenceSub(VecCoeficientes,VecRaices,grado,tol,maxiter)
+	call CorrigeSolucion(VecRaices,grado,Metodo,diferencia)
+	
+endif
 !Write (*,*) 'Ingrese la tolerancia'               !!Aca esta en rojo porque mientras lo vas probando es más facil ya dejarlos puestos
                                                     ! están más abajo los valores.
    !Read(*,*) tol
@@ -23,10 +47,6 @@ CALL ingresa_coeficientes(i,n,a)
 !Write (*,*) 'Ingrese la cantidad maxima de iteraciones'
    !Read(*,*) maxiter
 
-Write(*,*) a(n-1)
-Write(*,*) a(n)
-Write(*,*) a(0)
-Write(*,*) 'aaaaaaaaaaaa' !para ver si funciona
 
 
 
@@ -38,33 +58,56 @@ Write(*,*) 'aaaaaaaaaaaa' !para ver si funciona
 
 CONTAINS
 
-SUBROUTINE LeeCoeficientes(a,grado)
-integer i,n
-real(8) a(0:)
+function CoeficienteCero(VecCoeficientes,Grado)
+logical CoeficienteCero
+real(8) VecCoeficientes(:)
+integer Grado,i
+
+do while ((VecCoeficientes(i)/=0) .AND. (i<Grado))
+	i=i+1
+enddo
+
+CoeficienteCero=(Grado==i)
+
+
+
+endfunction
+SUBROUTINE LeeCoeficientes(VecCoeficientes,Grado)
+integer i,Grado
+real(8), allocatable :: VecCoeficientes(:)
 
 open(2,FILE="CoeficientesPolinomio.txt", action="read")
 read(2,*)grado
 
-allocate(a(grado))
+allocate(VecCoeficientes(grado))
 
-do i=0,grado 
-	read(2,*)a(i)
+do i=1,grado 
+	read(2,*)VecCoeficientes(i)
 enddo
 
 
 
 END SUBROUTINE
 
-subroutine QuotientDifference()    
+subroutine QuotientDifferenceSub(VecCoeficientes,VecRaices,tol,maxiter)
+!parametros 
+real(8) intent(inout)::VecCoeficientes(:),VecRaices(:)
+integer ,intent(in):: grado,maxiter
+real(8) ,intent(in)::error
+!var locales
+integer i
+real(8) error
+
+   
 !COMIENZA QD
-q=0.
+q=0.          !que carajo significan las variables
 e(n)=0.
 e(0)=0.
 
-!ITERACIÓN 0
-!CALL Iteracion_cero(q,n,e,i) !Todavía no existe esta subrutina, la puse porque por ahi la idea era ir llamando subrutinas pero
-                              ! creo que es mejor que en el programa se pueda explicar todo sin tener que ir y volver de la seccion 
-                              !Contains, para poder darle mas ritmo al explicarñ
+
+!CALL Iteracion_cero(q,n,e,i) 
+
+
  q(1)=-(a(n-1))/(a(n))
     do i=1, n-1
       e(i)=a(n-i-1)/a(n-i)
@@ -75,7 +118,9 @@ maxiter=20
 iter=0
 tol=0.0005
 error=2*tol
-!Resto de las iteraciones
+
+
+
 Do while (error>tol .and. maxiter>iter)
 
     Do i=1, n                  !Calculo la fila de entera en cada iteración
@@ -86,10 +131,7 @@ Do while (error>tol .and. maxiter>iter)
        e(i)=e(i)*q(i+1)/q(i)   ! Hay un error de tipeo en el pdf que figura q(i-1) pero en la tabla utiliza q(i+1), sino no da
     end do
    
-!Write(*,*) q
-!Write(*,*) e  !no me escribe e(0) pero siempre es igual a cero asi que meh, pasa que por defecto arranca con el elemento 1
-               !igual solo esto era solo para chequear que estén bien los resultados
-!Write(*,*)
+
      iter=iter+1
      error=maxval(abs(e))
 end do
